3c1757d47d44a1fb03c75d77c9263eacb08a7964
rgarcia101 <raquel_garcia96@yahoo.com>
2023-11-14 14:07:16 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import "dotenv/config";
+import session from "express-session";
+import Hello from "./hello.js"
+import Lab5 from './Lab5.js';
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
+import cors from "cors";
+const app = express();
+app.use(express.json());
+//app.use(cors());
+
+app.use(
+    cors({
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    })
+  );  
+CourseRoutes(app);
+ModuleRoutes(app);
+Lab5(app);
+Hello(app);
+app.listen(process.env.PORT || 4000);
---
+++ courses/routes.js
@@ -0,0 +1,61 @@
+import Database from "../Database/index.js";
+function CourseRoutes(app) {
+  app.get("/api/courses/:id", (req, res) => {
+    const { id } = req.params;
+    const course = Database.courses
+      .find((c) => c._id === id);
+    if (!course) {
+      res.status(404).send("Course not found");
+      return;
+    }
+    res.json(course);
+  });
+
+
+  app.put("/api/courses/:id", (req, res) => {
+    const { id } = req.params;
+    const index = Database.courses.findIndex((c) =>
+      c._id === id);
+      if (index === -1){
+        res.status(404).send("Course not found");
+        return;
+      }
+      Database.courses[index] = {
+        ...Database.courses[index],
+        ...req.body
+      };
+      res.json(200);
+  });
+
+
+    //const course = req.body;
+    // Database.courses = Database.courses.map((c) =>
+    //   c._id === id ? { c, ...course } : c
+    // );
+  //   console.log("printing all coursec after updating")
+  //   console.log(Database.courses);
+  //   res.sendStatus(204);
+  // });
+
+
+  app.delete("/api/courses/:id", (req, res) => {
+    const { id } = req.params;
+    Database.courses = Database.courses
+      .filter((c) => c._id !== id);
+    res.sendStatus(204);
+  });
+
+  app.post("/api/courses", (req, res) => {
+    const course = { ...req.body,
+      _id: new Date().getTime().toString() };
+    Database.courses.push(course);
+    res.send(course);
+  });
+
+  app.get("/api/courses", (req, res) => {
+    const courses = Database.courses;
+    res.json(courses);
+  });
+  
+}
+export default CourseRoutes;
