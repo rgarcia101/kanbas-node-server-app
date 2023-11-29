5b97237d151e4d2274ef9218e515f9c4248188fb
rgarcia101 <raquel_garcia96@yahoo.com>
2023-11-29 14:39:23 -0500

    updates

--- app.js
+++ app.js
@@ -35,7 +35,9 @@ session(sessionOptions)
 );
     
 UserRoutes(app);
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+mongoose.connect(CONNECTION_STRING);
+//mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 
 // app.use(
 //     cors({
