3b10a71031dbdc5276675811698c4c6d8d83eb0b
rgarcia101 <raquel_garcia96@yahoo.com>
2023-11-28 19:46:37 -0500

    pre section 4

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 import express from 'express';
+import session from "express-session";
 import "dotenv/config";
 import Hello from "./hello.js"
 import Lab5 from './Lab5.js';
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+    cors({
+      credentials: true,
+      origin: "http://localhost:3000",
+    })
+   );
+const sessionOptions = {
+secret: "any string",
+resave: false,
+saveUninitialized: false,
+};
+app.use(
+session(sessionOptions)
+);
+    
+UserRoutes(app);
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 
 // app.use(
 //     cors({
