21ba2e2c0e863c739f3e4b0f4edee22f610babde
rgarcia101 <raquel_garcia96@yahoo.com>
2023-11-29 08:16:20 -0500

    added changes

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(
     cors({
       credentials: true,
-      origin: "http://localhost:3000",
+      origin: process.env.FRONTEND_URL
     })
    );
 const sessionOptions = {
@@ -22,6 +22,14 @@ secret: "any string",
 resave: false,
 saveUninitialized: false,
 };
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+  }
+  
 app.use(
 session(sessionOptions)
 );
